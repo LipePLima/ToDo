a47cd6389deeefbceecbde9ab72655e89c5027da
Felipe Lima <102830741+LipePLima@users.noreply.github.com>
2023-02-09 15:19:49 -0300

    feat: Adicionando componente list; Style: Adicionando pseudoclasse active com background-color para os botões input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { BodyComponent } from './components/body/body.component';
+import { ListComponent } from './components/list/list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     BodyComponent,
+    ListComponent,
   ],
   imports: [
     BrowserModule,
